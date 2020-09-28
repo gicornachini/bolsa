0cfef502bfb88abbce4fdaba0854d06dfc6138b1
Giovanni Cornachini <giovannicornachini@gmail.com>
2020-09-27 23:15:48 -0300

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setuptools.setup(
     name='bolsa',
-    version='0.0.6',
+    version='0.0.7',
     packages=setuptools.find_packages(),
     python_requires='>=3.8.*',
     author='Giovanni Cornachini',
