52d7ea44f05d9e24812560b332d34e8c538645af
Giovanni Cornachini <giovannicornachini@gmail.com>
2020-11-08 22:19:24 -0300

    fix: captcha message error and improve logging

--- bolsa/captcha/services/TwoCaptchaResolverService.py
+++ bolsa/captcha/services/TwoCaptchaResolverService.py
@@ -1,7 +1,17 @@
+import logging
+
 from twocaptcha import TwoCaptcha
 from twocaptcha.api import ApiException
-from bolsa.captcha.CaptchaResolverServiceInterface import CaptchaResolverServiceInterface
-from bolsa.captcha.exceptions.CaptchaResolverException import CaptchaResolverException
+
+from bolsa.captcha.CaptchaResolverServiceInterface import (
+    CaptchaResolverServiceInterface
+)
+from bolsa.captcha.exceptions.CaptchaResolverException import (
+    CaptchaResolverException
+)
+
+logger = logging.getLogger(__name__)
+
 
 class TwoCaptchaResolverService(CaptchaResolverServiceInterface):
 
@@ -10,9 +20,18 @@ class TwoCaptchaResolverService(CaptchaResolverServiceInterface):
 
     async def resolve(self, site_key, url):
         solver = TwoCaptcha(self._credential)
-
+        logger.info(
+            'Resolving captcha.'
+        )
         try:
             solvedcaptcha = solver.recaptcha(site_key, url)
         except ApiException as error:
-            raise CaptchaResolverException(f'Erro ao tentar resolver captcha: {e}')
+            raise CaptchaResolverException(
+                f'Erro ao tentar resolver captcha: {error}'
+            )
+
+        logger.info(
+            'Captcha resolved.'
+        )
+
         return solvedcaptcha['code']
